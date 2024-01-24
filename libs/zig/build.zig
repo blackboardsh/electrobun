abc33130dfc549c6ec591aec2ae61a89e5b0b895
YoavCodes <hey@yoav.codes>
2024-01-24 11:28:55 -0500

    cleanup build.zig

--- libs/zig/build.zig
+++ libs/zig/build.zig
@@ -1,28 +1,10 @@
-// const std = @import("std");
-// const objc = @import("zig-objc/build.zig");
-
-// pub fn build(b: *std.Build) !void {
-//     // const exe = b.addExecutable("myapp", "src/main.zig");
-//     b.addExecutable(.{
-//         .name = "webview_wrapper",
-//         .root_source_file = .{ .path = "src/main.zig" },
-//         // .target = b.host,
-//     });
-
-//     // exe.addPackage(objc.pkg);
-// }
-
 const std = @import("std");
 
 pub fn build(b: *std.Build) void {
     const exe = b.addExecutable(.{
         .name = "webview",
         .root_source_file = .{ .path = "main.zig" },
-        // .target = b.host,
     });
-    // exe.addPackage(objc.pkg);
-    // const objcIncludePath = std.Build.LazyPath.relative("../zig-objc/src");
-    // exe.addIncludePath(objcIncludePath);
 
     // need to link objective c runtime in order for zig-objc to work
     exe.linkSystemLibrary("objc");
