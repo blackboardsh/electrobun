80bf90276c6fa2d6c90cade8527e66b8bd96f1d2
YoavCodes <hey@yoav.codes>
2024-03-05 09:54:08 -0500

    install buntypes and update rpc-anywhere

--- example/build/dev.app/Contents/MacOS/bun/index.js
+++ example/build/dev.app/Contents/MacOS/bun/index.js
@@ -59,6 +59,10 @@ var missingTransportMethodError = function(methods, action) {
   return new Error(`This RPC instance cannot ${action} because the transport did not provide one or more of these methods: ${methodsString}`);
 };
 function _createRPC(options = {}) {
+  let debugHooks = {};
+  function _setDebugHooks(newDebugHooks) {
+    debugHooks = newDebugHooks;
+  }
   let transport = {};
   function setTransport(newTransport) {
     if (transport.unregisterHandler)
@@ -87,6 +91,8 @@ function _createRPC(options = {}) {
     setTransport(options.transport);
   if (options.requestHandler)
     setRequestHandler(options.requestHandler);
+  if (options._debugHooks)
+    _setDebugHooks(options._debugHooks);
   let lastRequestId = 0;
   function getRequestId() {
     if (lastRequestId <= MAX_ID)
@@ -113,6 +119,7 @@ function _createRPC(options = {}) {
           requestTimeouts.delete(requestId);
           reject(new Error("RPC request timed out."));
         }, maxRequestTime));
+      debugHooks.onSend?.(request2);
       transport.send(request2);
     });
   }
@@ -133,6 +140,7 @@ function _createRPC(options = {}) {
       id: message,
       payload
     };
+    debugHooks.onSend?.(rpcMessage);
     transport.send(rpcMessage);
   }
   const send = new Proxy(sendFn, {
@@ -166,6 +174,7 @@ function _createRPC(options = {}) {
       messageListeners.delete(message);
   }
   async function handler(message) {
+    debugHooks.onReceive?.(message);
     if (!("type" in message))
       throw new Error("Message does not contain a type.");
     if (message.type === "request") {
@@ -190,6 +199,7 @@ function _createRPC(options = {}) {
           error: error.message
         };
       }
+      debugHooks.onSend?.(response);
       transport.send(response);
       return;
     }
@@ -226,7 +236,8 @@ function _createRPC(options = {}) {
     sendProxy,
     addMessageListener,
     removeMessageListener,
-    proxy
+    proxy,
+    _setDebugHooks
   };
 }
 var MAX_ID = 10000000000;
