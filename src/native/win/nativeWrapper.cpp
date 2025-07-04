7d0bf075ae3d37a9ac0dc1e0e1434fda015395fe
YoavCodes <hey@yoav.codes>
2025-07-03 23:45:29 -0400

    fixes

--- src/native/win/nativeWrapper.cpp
+++ src/native/win/nativeWrapper.cpp
@@ -117,6 +117,7 @@ void handleViewsSchemeRequest(ICoreWebView2WebResourceRequestedEventArgs* args,
                              uint32_t webviewId);
 std::string loadViewsFile(const std::string& path);
 std::string getMimeTypeForFile(const std::string& path);
+ELECTROBUN_EXPORT void stopWindowMove();
 
 void log(const std::string& message) {
     // Get current time
