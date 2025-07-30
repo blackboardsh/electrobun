490ec970da31258ebad3c1ce2c13841b2753e3c5
YoavCodes <hey@yoav.codes>
2025-07-30 13:40:22 -0400

    baseline fix other zig binaries

--- src/extractor/build.zig
+++ src/extractor/build.zig
@@ -2,8 +2,13 @@ const std = @import("std");
 
 pub fn build(b: *std.Build) void {
     // zig build -Doptimize=Debug to enable debug mode
-    const target = b.standardTargetOptions(.{});
-    // const optimize = b.standardOptimizeOption(.{});
+    var target = b.standardTargetOptions(.{});
+    
+    // Force baseline CPU features for Windows x64 to ensure ARM64 emulation compatibility
+    if (target.result.os.tag == .windows and target.result.cpu.arch == .x86_64) {
+        target.result.cpu.features = std.Target.x86.baseline(.x86_64).features;
+    }
+    
     const optimize = b.standardOptimizeOption(.{});
 
     // todo: should probably rename webview to something else
