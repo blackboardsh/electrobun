e7ddf2daffa761ed52cf3045f1b499fe9b81a7f9
Yoav <hey@yoav.codes>
2026-01-25 19:20:32 -0500

    bump zig-bsdiff

--- package/build.ts
+++ package/build.ts
@@ -728,7 +728,7 @@ async function vendorZig() {
 }
 
 async function vendorBsdiff() {
-    const BSDIFF_VERSION = '0.1.14';
+    const BSDIFF_VERSION = '0.1.15';
     const bsdiffDir = join(process.cwd(), 'vendors', 'zig-bsdiff');
     const bsdiffBin = join(bsdiffDir, 'bsdiff' + binExt);
     const bspatchBin = join(bsdiffDir, 'bspatch' + binExt);
