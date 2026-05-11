28ed4628ab1aabb426c64cf76cf4d9720fc008d7
Yoav <hey@yoav.codes>
2026-05-11 12:27:50 -0400

    v1.18.4-beta.3

--- kitchen/electrobun.config.ts
+++ kitchen/electrobun.config.ts
@@ -8,7 +8,7 @@ export default {
 	app: {
 		name: "Electrobun Kitchen Sink",
 		identifier: "sh.blackboard.electrobun-kitchen",
-		version: "1.18.4-beta.2",
+		version: "1.18.4-beta.3",
 		urlSchemes: ["electrobun-playground"],
 	},
 	runtime: {
