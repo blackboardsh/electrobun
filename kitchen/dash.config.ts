20c55237dd3305946a6fc51f77ea23157aa054e4
Yoav <hey@yoav.codes>
2026-07-26 15:54:27 -0400

    build: pin Dash signing fix

--- kitchen/dash.config.ts
+++ kitchen/dash.config.ts
@@ -1,2 +1,2 @@
-// @dash cli=0.2.0-canary.1 cottontail=0.1.1-canary.2
+// @dash cli=0.2.0-canary.2 cottontail=0.1.1-canary.2
 export default {};
--- package/dash.config.ts
+++ package/dash.config.ts
@@ -1,4 +1,4 @@
-// @dash cli=0.2.0-canary.1 cottontail=0.1.1-canary.2
+// @dash cli=0.2.0-canary.2 cottontail=0.1.1-canary.2
 export default {
 	scripts: {
 		start: "dash src/sdks/bun/index.ts",
