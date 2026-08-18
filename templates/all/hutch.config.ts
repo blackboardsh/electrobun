56caf3bdce415c65ca51a62f51fee8236f52f253
Yoav <hey@yoav.codes>
2026-08-18 18:49:53 -0400

    v2.0.1-beta.18

--- templates/all/hutch.config.ts
+++ templates/all/hutch.config.ts
@@ -1,7 +1,7 @@
 // @hutch cli=0.13.0 cottontail=0.5.0
 export default {
 	electrobun: {
-		version: "2.0.1-beta.17",
+		version: "2.0.1-beta.18",
 	},
 	scripts: {
 		start: ["hutch", "electrobun", "dev"],
