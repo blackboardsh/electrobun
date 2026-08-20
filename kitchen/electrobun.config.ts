c188c243786889948559de7fd22dbb6cd281eaaf
Yoav <hey@yoav.codes>
2026-08-20 14:59:54 -0400

    v2.0.1-beta.28

--- kitchen/electrobun.config.ts
+++ kitchen/electrobun.config.ts
@@ -20,7 +20,7 @@ export default {
 	app: {
 		name: appName,
 		identifier: appIdentifier,
-		version: "2.0.1-beta.27",
+		version: "2.0.1-beta.28",
 		urlSchemes: ["electrobun-playground"],
 	},
 	runtime: {
--- kitchen/hutch.config.ts
+++ kitchen/hutch.config.ts
@@ -1,7 +1,7 @@
 // @hutch cli=0.24.2 cottontail=0.5.0
 export default {
 	electrobun: {
-		version: "2.0.1-beta.27",
+		version: "2.0.1-beta.28",
 	},
 	packageManager: "npm",
 	scripts: {
