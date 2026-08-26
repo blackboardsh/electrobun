aad13726a5e0001870cec37b4d991843790b06e0
YoavCodes <hey@yoav.codes>
2026-08-26 14:29:06 -0400

    v2.0.2-beta.11

--- kitchen/electrobun.config.ts
+++ kitchen/electrobun.config.ts
@@ -20,7 +20,7 @@ export default {
 	app: {
 		name: appName,
 		identifier: appIdentifier,
-		version: "2.0.2-beta.10",
+		version: "2.0.2-beta.11",
 		urlSchemes: ["electrobun-playground"],
 	},
 	runtime: {
