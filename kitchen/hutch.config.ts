33376c0ba4285d9e26f59820af4ce0e647b0f649
Yoav <hey@yoav.codes>
2026-08-25 22:10:35 -0400

    release: bundle Cottontail canary in beta.7

--- kitchen/hutch.config.ts
+++ kitchen/hutch.config.ts
@@ -1,7 +1,7 @@
 // @hutch cli=0.26.0-canary.4 cottontail=0.6.0-canary.11
 export default {
 	electrobun: {
-		version: "2.0.2-beta.6",
+		version: "2.0.2-beta.7",
 	},
 	packageManager: "npm",
 	scripts: {
