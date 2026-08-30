61b514f2b787d40c8ecdc87c482d8b97c6a33d7b
Yoav Givati <hey@yoav.codes>
2026-08-29 17:10:02 -0700

    v2.0.2-beta.13

--- kitchen/electrobun.config.ts
+++ kitchen/electrobun.config.ts
@@ -20,7 +20,7 @@ export default {
 	app: {
 		name: appName,
 		identifier: appIdentifier,
-		version: "2.0.2-beta.12",
+		version: "2.0.2-beta.13",
 		urlSchemes: ["electrobun-playground"],
 	},
 	runtime: {
