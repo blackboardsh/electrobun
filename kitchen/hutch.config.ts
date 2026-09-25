66ee6392bc8413cded22e65c685c02ea48f2c728
Yoav <hey@yoav.codes>
2026-09-25 00:16:38 -0400

    v2.0.2-beta.30

--- kitchen/hutch.config.ts
+++ kitchen/hutch.config.ts
@@ -1,7 +1,7 @@
 // @hutch cli=0.27.0-canary.13 cottontail=0.7.0-canary.14
 export default {
 	electrobun: {
-		version: "2.0.2-beta.29",
+		version: "2.0.2-beta.30",
 	},
 	packageManager: "npm",
 	scripts: {
